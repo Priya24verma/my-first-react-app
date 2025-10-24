0eb33482eb326ea27f1c144c0640eeb955fefed7
Priya24verma <priya24verma@example.com>
2025-10-24 20:43:46 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-//import About from './components/About';     
+import About from './components/About';     
 import React,{useState} from 'react';
-//import {
-//BrowserRouter as Router,
-//Switch,
-//Route,
-//Link
-//} from "react-router-dom";
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+Link
+} from "react-router-dom";
 
 function App() {
 
@@ -32,43 +32,43 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode is enabled", "success ");
-      document.title = 'Textutils - Dark mode ';
+      //document.title = 'Textutils - Dark mode ';
    
       setInterval(() =>{
          document.title = 'Textutil is Amazing mode ';
       },2000);
 
       setInterval(() =>{
+
          document.title = 'Install textutil now !! ';
       },1500);
       
-
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode is enabled","success");
-      document.title = 'Textutils - Light mode ';
+      //document.title = 'Textutils - Light mode ';
     }
   }
   return (
     <>
-    {/*<Router>*/}
+    <Router>
     <Navbar title="TextUtils"mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert}/>
     <div className="container my-3">
-              {/*<Switch>*/}
+              <Switch>
                {/* /user --> component 1
                 /users /home --> component 2 */}
-          {/*<Route exact path="/about">
-            <About />
-          </Route>*/}
-          {/*<Route exact path="/">*/}
-                  <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode} />
-          {/*</Route>*/}
-        {/*</Switch>*/}
+          <Route exact path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route exact path="/">
+                  <TextForm showAlert={showAlert} heading="Textutils - Word Counter, Character Counter, Remove extra spaces" mode={mode} />
+          </Route>
+        </Switch>
     </div>
-    {/*</Router>*/}
+    </Router>
 
 </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
 
+
     const handleUpClick = ()=>{
         console.log("Uppercase was clicked"+ text);
         let newText = text.toUpperCase();
@@ -25,6 +26,7 @@ export default function TextForm(props) {
       let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Text is copied","success");
       
     }
@@ -54,25 +56,24 @@ export default function TextForm(props) {
     <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1> 
 <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#042743':'white',color: props.mode==='dark'?'white':'#042743'}}id="myBox" rows="8"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white',color: props.mode==='dark'?'white':'#042743'}}id="myBox" rows="8"></textarea>
 </div>
-  <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-  <button type="button" className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lowercase </button>
-  <button type="button" className="btn btn-primary mx-2" onClick={handleClearClick}>Clear the text </button>
+  <button type="button" disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to uppercase</button>
+  <button type="button" disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to lowercase </button>
+  <button type="button" disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear the text </button>
   {/* <button type="button" className="btn btn-primary mx-2" onClick={handleTitleClick}>Title the text </button> */}
-    <button type="button" className="btn btn-primary mx-2" onClick={handleCopy}>Copy the text </button>
-    <button type="button" className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove the extra space </button>
+    <button type="button" disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy the text </button>
+    <button type="button" disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>Remove the extra space </button>
+  </div>
 
-
-  
-    </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>your text summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes read </p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read </p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"enter the something above to preview here"}</p>
+        <p>{text.length>0?text:"Write something above to preview here"}</p>
     </div>
+    
     </>
   )
 } 
